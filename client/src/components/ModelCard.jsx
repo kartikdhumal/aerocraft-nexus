363f63351592074d507eb1efe5b3a14371760827
kartik <kartikdhumal24@gmail.com>
2024-02-29 20:42:47 +0530

    fixed review

--- client/src/components/ModelCard.jsx
+++ client/src/components/ModelCard.jsx
@@ -414,7 +414,7 @@ function ModelCard() {
                         {!loading && reviews.length > 0 && reviews.map(review => (
                             <div className="relative grid grid-cols-1 gap-4 p-4 mb-8 border rounded-lg bg-white shadow-lg">
                                 <div className="relative flex gap-4">
-                                    <img src={user} className="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20" alt="" loading="lazy"></img>
+                                    <img src={user} className="relative rounded-lg lg:-top-2 sm:-top-2 vsm:-top-2 bg-white border lg:h-14 lg:w-14 sm:h-14 sm:w-14 vsm:h-14 vsm:w-14" alt="" loading="lazy"></img>
                                     <div className="flex flex-col w-full">
                                         <div className="flex flex-row justify-between">
                                             <p className="relative text-xl whitespace-nowrap truncate overflow-hidden">{getUserName(review.userId)}</p>
